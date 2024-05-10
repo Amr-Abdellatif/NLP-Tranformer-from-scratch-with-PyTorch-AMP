9fda6cd6ce9c12e0ae321499276a6ed1c86b5e8a
Amr Abdellatif <amrosama42@gmail.com>
2024-05-10 23:29:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,11 +36,3 @@ def latest_weights_file_path(config):
         return None
     weights_files.sort()
     return str(weights_files[-1])
-
-
-# def get_weights_file_path(config,epoch:str):
-#     model_folder = config['model_folder']      
-#     model_basename = config['model_basename']
-#     model_filename = f"{model_basename}{epoch}.pt"
-#     return str(Path('.')/model_folder/model_filename)
-
